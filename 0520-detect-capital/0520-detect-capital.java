b1f0d243f25cc9edaf30aa8c1c537bbaf4d09fe2
Rudra <rudrapratapsingh1357serf@gmail.com>
2026-08-22 14:49:25 +0530

    Time: 1 ms (89.44%), Space: 43.2 MB (44.05%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.equals(word.toUpperCase())) {
+            return true;
+        }
+        if (word.equals(word.toLowerCase())) {
+            return true;
+        }
+        if (Character.isUpperCase(word.charAt(0))) {
+            for (int i = 1; i < word.length(); i++) {
+                if (Character.isUpperCase(word.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
